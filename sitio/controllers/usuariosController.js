fb3cb8732b9da254fbbf276562639c649f96fb57
Paulina <fridman.paulinasofia@gmail.com>
2020-09-10 00:15:33 -0300

    controller

--- sitio/controllers/usuariosController.js
+++ sitio/controllers/usuariosController.js
@@ -1,3 +1,6 @@
+let dbProducts = require('../data/dbDataNew');
+let dbUsers = require('../data/users.json');
+const users = require('../data/users');
 
 module.exports = { //exporto un objeto literal con todos los metodos
     registro:function(req,res){
@@ -5,6 +8,22 @@ module.exports = { //exporto un objeto literal con todos los metodos
             css:"style.css"
         })
     },
+    processRegistro:function(req, res){
+        let errors= validationResult(req);
+        let lastid= users.length;
+        if(errors.isEmpty()){
+            let nuevoUsuario={
+                id:lastid +1,
+                nombre: req.body.nombre,
+                apellido: req.body.apellido,
+                email: req.body.apellido,
+                avatar:req.files[0].filename,
+                pass: bcrypt.hashSync(req.body.pass,10),
+                rol:"user",
+            };
+        }
+    },
+
     ingreso: function(req, res) {
         res.render('formIngreso',{
             css:"formIngreso.css"
